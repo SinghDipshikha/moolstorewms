0b170db1e1adec8802515357703d7e32be2464a5
Dipshikha Singh <dipshikhasingh@Dipshikhas-MacBook-Air.local>
2024-05-19 10:42:10 +0530

    Latest Changes

--- lib/Security Guard/Model/SecurityGuard/person.dart
+++ lib/Security Guard/Model/SecurityGuard/person.dart
@@ -7,14 +7,11 @@ part 'person.g.dart';
 class Person with _$Person {
   const factory Person({
     int? id,
-   
     String? person_name,
-     String? person_phone,
+    String? person_phone,
     String? status,
     DateTime? created_at,
-   
   }) = _GetAllPersonsBySecurityGaurd;
 
   factory Person.fromJson(Map<String, Object?> json) => _$PersonFromJson(json);
 }
- 
