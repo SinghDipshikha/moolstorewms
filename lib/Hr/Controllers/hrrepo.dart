67b2c27d2dd0fe6473915111fd65ecaeae8f31d1
Dispshikha Singh <dipshikha0201@gmail.com.com>
2024-07-09 15:34:19 +0530

    Latest Changes

--- lib/Hr/Controllers/hrrepo.dart
+++ lib/Hr/Controllers/hrrepo.dart
@@ -1,6 +1,11 @@
 import 'package:get/get.dart';
 import 'package:moolwmsstore/Common%20Data/api/api_client.dart';
 import 'package:moolwmsstore/Hr/Controllers/hrController.dart';
+import 'package:moolwmsstore/Hr/Model/Employee/addBankDetails.dart';
+import 'package:moolwmsstore/Hr/Model/Employee/addCareerDetail.dart';
+import 'package:moolwmsstore/Hr/Model/Employee/addEducationDetails.dart';
+import 'package:moolwmsstore/Hr/Model/Employee/addReferralDetails.dart';
+import 'package:moolwmsstore/Hr/Model/Employee/personaldetails.dart';
 import 'package:moolwmsstore/Hr/Model/attendance.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -8,6 +13,8 @@ class HrRepo {
   ApiClient apiClient;
   final SharedPreferences sharedPreferences;
   HrRepo({required this.sharedPreferences, required this.apiClient});
+  List<PersonalDetailsResponse> getPersonalDetailsList = [];
+  List<AddCareerDetail> getCareerDetailsList = [];
 
   Future<List<AttendanceEntry>?> getAllAttendance({
     required int recordsPerPage,
@@ -28,4 +35,94 @@ class HrRepo {
     }
     return null;
   }
+
+/////////// Get Personal Details/////////
+  Future<PersonalDetailsResponse?> getPersonalDetails() async {
+    var res = await apiClient.postData("hr/getBasicInformationById",
+        {"user_id": Get.find<HRController>().currentlySeletedEmployee!.id},
+        passhandlecheck: true);
+    if (res.data["message"] == "Basic information found") {
+      return (res.data["result"] as List)
+          .map((e) => PersonalDetailsResponse.fromJson(e))
+          .toList()[0];
+    } else {
+      return null;
+    }
+  }
+
+  /////////////////Get Career Details///////////////
+  Future<List<AddCareerDetail>?> getCareerDetails() async {
+    var res = await apiClient.getData(
+      "hr/getBasicInformationById/${Get.find<HRController>().currentlySeletedEmployee!.id}",
+    );
+    if (res.data["message"] == "Career details found") {
+      return getCareerDetailsList = (res.data["result"] as List)
+          .map((e) => AddCareerDetail.fromJson(e))
+          .toList();
+    } else {
+      return null;
+    }
+  }
+
+  Future<AddEducationDetail?> getEducationDetails() async {
+    var res = await apiClient.postData("hr/getEducationInformationById",
+        {"user_id": Get.find<HRController>().currentlySeletedEmployee!.id});
+    if (res.data["message"] == "Education information found") {
+      return (res.data["result"] as List)
+          .map((e) => AddEducationDetail.fromJson(e))
+          .toList()
+          .last;
+    } else {
+      return null;
+    }
+  }
+
+  /////////////////Get Bank Details///////////////
+  Future<AddBankDetails?> getbankDetails() async {
+    var res = await apiClient.getData(
+      "hr/getBasicInformationById/${Get.find<HRController>().currentlySeletedEmployee!.id}",
+    );
+    if (res.data["message"] == "Bank details found") {
+      //   return getCareerDetailsList = (res.data["result"] as List)
+      //       .map((e) => AddCareerDetail.fromJson(e))
+      //       .toList();
+    } else {
+      return null;
+    }
+    return null;
+  }
+
+  Future<AddReferralDetail?> getReferDetails() async {
+    var res = await apiClient.postData("hr/getUserReferralsById",
+        {"user_id": Get.find<HRController>().currentlySeletedEmployee!.id});
+    if (res.data["message"] == "Referral details found") {
+      return (res.data["result"] as List)
+          .map((e) => AddReferralDetail.fromJson(e))
+          .toList()
+          .last;
+    } else {
+      return null;
+    }
+  }
+
+  // getCareerDetails(var userId) {
+  //   apiClient
+  //       .getData(
+  //     "hr/getCareerDetailsByUserId/$userId",
+  //   )
+  //       .then((value) {
+  //     if (value.data["message"] == "career details found") {
+  //       Snacks.greenSnack("Career Details");
+  //       List x = value.data["result"];
+  //       getCareerDetailsList =
+  //           x.map((e) => AddCareerDetail.fromJson(e)).toList();
+
+  // isLoading = false;
+  // update();
+  //     } else {
+  //       isLoading = false;
+  //       update();
+  //     }
+  //   });
+  // }
 }
--- lib/Hr/Model/referralDetailsRequest.dart
+++ lib/Hr/Model/referralDetailsRequest.dart
@@ -6,19 +6,16 @@ part 'referralDetailsRequest.g.dart';
 @freezed
 class ReferralDetailsRequest with _$ReferralDetailsRequest {
   const factory ReferralDetailsRequest({
- int ?user_id,
-    int ?updated_by,
-    String ? name,
-    String ?company,
-    String ?designation,
-    String ?email_id,
-    String ?mobile_number,
-    String ?address,
-
-    
+    int? user_id,
+    int? updated_by,
+    String? name,
+    String? company,
+    String? designation,
+    String? email_id,
+    String? mobile_number,
+    String? address,
   }) = _ReferralDetailsRequest;
 
   factory ReferralDetailsRequest.fromJson(Map<String, Object?> json) =>
       _$ReferralDetailsRequestFromJson(json);
 }
- 
